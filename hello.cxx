abcdbfc975d31e29380651e93d4c1f50fe478722
Dimitar Trendafilov <dimitar@coherent-labs.com>
2016-10-08 13:46:01 +0300

    Refactor

--- hello.cxx
+++ hello.cxx
@@ -1,7 +1,11 @@
 #include <cstdio>
 
+int the_answer() {
+    return 42;
+}
+
 int main()
 {
-    std::printf("The answer is %d\n", 42);
+    std::printf("The answer is %d\n", the_answer());
     return 0;
 }
